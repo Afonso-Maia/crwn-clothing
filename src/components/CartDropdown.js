92d4f89af82f800972d6f4f495e7f3f42d5d2971
Afonso Maia <affonsomaia@gmail.com>
2020-09-21 09:14:02 -0300

    Updated aplication with reselect memoized selectors

--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { selectCartItems } from '@redux/cart/cart.selectors'
 
 import Button from '@components/Button'
 import StyledCartDropdown from '@styled/StyledCartDropdown'
 import CartItem from './CartItem'
 
 const CartDropdown = () => {
-  const cartItems = useSelector(state => state.cart.cartItems)
+  const cartItems = useSelector(selectCartItems)
   return (
     <StyledCartDropdown>
       <div className="cart-items">
--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { toggleCartHidden } from '../redux/cart/cart.actions'
+import { selectCartItemsCount } from '@redux/cart/cart.selectors'
 
 import { ReactComponent as ShopingIcon } from '@assets/shopping-bag.svg'
 import StyledCartIcon from '@styled/StyledCartIcon'
 
 const CartIcon = () => {
   const dispatch = useDispatch()
+  const itemsQuantity = useSelector(selectCartItemsCount)
+
   return (
     <StyledCartIcon onClick={() => dispatch(toggleCartHidden())}>
       <ShopingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemsQuantity}</span>
     </StyledCartIcon>
   )
 }
